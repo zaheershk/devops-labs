15c9ea787e3754e44f19c9410d65170f09d7cf93
zaheershk <zaheer.azad@gmail.com>
2020-05-13 14:14:53 +0530

    initial commit for docker-nginx-local

---
+++ docker/simple-demo/main.tf
@@ -0,0 +1,17 @@
+﻿provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
